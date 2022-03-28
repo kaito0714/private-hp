773b2335b9637fed7eb6ba992db8d2e1b6d6438c
Kaito Okada <56987800+kaito0714@users.noreply.github.com>
2022-03-28 17:45:22 +0900

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+document.write(`
+<div id="footer">
+    <ul>
+        <li><a href="#"><img src="images/hatenablog.png" alt=""></a></li>
+        <li><a href="#"><img src="images/GitHub.png" alt=""></a></li>
+        <li><a href="#"><img src="images/twitter.png" alt=""></a></li>
+    </ul>
+</div>
+`);
